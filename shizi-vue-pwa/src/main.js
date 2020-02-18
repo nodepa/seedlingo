60a9c3dae6e72837724e4af4cf39e94b0e1716a8
toshify <4579559+toshify@users.noreply.github.com>
2020-02-18 09:47:24 +0800

    Reduces to only react and vue test projects

--- shizi-vue-pwa/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
