d676613064169f0018af6f579e8a0e58919b57e6
toshify <4579559+toshify@users.noreply.github.com>
2020-02-25 14:19:17 +0800

    Adds Amplify notest to setup

--- ssc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# requires openssl > 1.1.1
-openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
-  -keyout selfssl.key -out selfssl.crt -subj "/CN=localhost" \
-  -addext "subjectAltName=DNS:localhost,IP:10.0.0.1,IP:127.0.0.1"
