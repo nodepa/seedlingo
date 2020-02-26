689b7c18f15e4a7bd0f071054715f35a58187d6e
toshify <4579559+toshify@users.noreply.github.com>
2020-02-26 22:07:45 +0800

    [#7] Dev uses CI/CD to increase speed
    PR #7
    Issue  #4
    
    Requires: Configuring [AWS Amplify]
    (https://console.aws.amazon.com/amplify/home) to call the correct
    script commands found in `package.json`.
    `amplify.yml` has not been added to source.
    
    * Adds `cypress` to the `react` front end
    * Adds `cypress.json` to the both front ends
    * Adds header with app name and framework to homepage with test

--- react/src/App.js
+++ react/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>立爱识字 on React.js</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
