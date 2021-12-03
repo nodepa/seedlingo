e30dbf3b35d52dcb4fdc18732182973d75d65d95
toshify <4579559+toshify@users.noreply.github.com>
2021-12-03 13:34:36 +0100

    fix: Add range handling

--- app/src/service-worker.js
+++ app/src/service-worker.js
@@ -1,9 +1,20 @@
 import { setCacheNameDetails } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
+import { RangeRequestPlugin } from 'workbox-range-requests';
+
 setCacheNameDetails({ prefix: 'seedling' });
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+// registerRoute(
+//   ({ url }) => url.pathname.endsWith('.mp3'),
+//   new CacheFirst({
+//     plugins: [new RangeRequestPlugin()],
+//   }),
+// );
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
