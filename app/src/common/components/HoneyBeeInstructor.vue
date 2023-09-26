78e4e1b5a5b2b1e7a2fef6962348dbcf06c3e1a0
toshify <4579559+toshify@users.noreply.github.com>
2023-09-26 12:14:06 +0200

    refactor: Move honeybee-code into reusable component
    Because we should reuse the honeybee as an app-wide engaging instructor,
    and we need to de-tangle its code from the comprehension exercise,
    
    this commit will:
    - move honeybee functionality into a separate compontent
    - refactor code to be DRY-er
    
    **Certification**
    - [X] I certify that <!-- Check the box to certify: [X] -->
    - I have read the [contributing guidelines](
      https://github.com/nodepa/seedling/blob/main/.github/CONTRIBUTING.md)
    - I license these contributions to the public under Seedling's
      [LICENSE](https://github.com/nodepa/seedling/blob/main/LICENSE.md)
      and have the rights to do so.
    
    Signed-off-by: toshify <4579559+toshify@users.noreply.github.com>

---
+++ app/src/common/components/HoneyBeeInstructor.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { ExerciseAudio } from '@/common/types/ExerciseAudioType';
+import Instructor from '@/common/icons/HoneyBee.svg';
+import RippleAnimation from '@/common/animations/RippleAnimation.vue';
+import { computed, ref } from 'vue';
+
+interface Props {
+  audio?: ExerciseAudio;
+  text?: string;
+  centered?: boolean;
+  sizeWhenQuiet?: number;
+  sizeWhenSpeaking?: number;
+}
+withDefaults(defineProps<Props>(), {
+  audio: undefined,
+  text: undefined,
+  centered: false,
+  sizeWhenQuiet: 48,
+  sizeWhenSpeaking: 164,
+});
+defineEmits(['click']);
+const card = ref();
+const cardHeight = computed(() => {
+  return +getComputedStyle(card.value).height.replace('px', '');
+});
+</script>
+<template>
+  <div>
+    <!-- Honeybee icon -->
+    <button
+      :style="{
+        position: 'relative',
+        zIndex: 1,
+        left: centered
+          ? `calc(50% - ${
+              audio?.playing ? sizeWhenSpeaking / 2 : sizeWhenQuiet / 2
+            }px)`
+          : '0px',
+        width: audio?.playing ? `${sizeWhenSpeaking}px` : `${sizeWhenQuiet}px`,
+        height: `${audio?.playing ? sizeWhenSpeaking : sizeWhenQuiet}px`,
+        transition: '1s ease-in-out',
+        color: 'var(--ion-card-background)',
+        backgroundColor: 'var(--ion-color-card)',
+        border: '2px solid var(--ion-color-primary)',
+        borderRadius: '50%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        cursor: 'pointer',
+      }"
+      @click="$emit('click')"
+    >
+      <ion-icon
+        :icon="Instructor"
+        style="width: 80%; height: 80%; transition: 1s ease-in-out"
+        aria-hidden="false"
+      />
+      <RippleAnimation :playing="audio?.playing" />
+    </button>
+    <!-- Honeybee text -->
+    <div
+      :style="{
+        width: '100%',
+        height: audio?.playing ? `${cardHeight}px` : '0px',
+        marginTop: '-20px',
+        scale: audio?.playing ? 1 : 0,
+        translate: centered
+          ? undefined
+          : audio?.playing
+          ? '0% 0%'
+          : 'calc(-50% - 10px) calc(-50% - 10px)',
+        transition: '1s ease-in-out',
+      }"
+    >
+      <ion-card ref="card" @click="$emit('click')">
+        <ion-card-content style="font-size: 1rem; line-height: normal">
+          <span>{{ text }} </span>
+        </ion-card-content>
+      </ion-card>
+    </div>
+  </div>
+</template>
