f4275254084524625cb352b2ca5e035097abdbf1
toshify <4579559+toshify@users.noreply.github.com>
2023-12-11 20:30:05 +0100

    infra: Update dependencies (#441)
    Because we want dependencies to be up to date
    to prevent security issues
    and to give users the best possible experience
    
    this commit will:
    - update dependencies to the latest available version
    
    **Certification**
    - [X] I certify that <!-- Check the box to certify: [X] -->
    - I have read the [contributing guidelines](
      https://github.com/nodepa/seedling/blob/main/.github/CONTRIBUTING.md)
    - I license these contributions to the public under Seedling's
      [LICENSE](https://github.com/nodepa/seedling/blob/main/LICENSE.md)
      and have the rights to do so.
    
    Signed-off-by: toshify <4579559+toshify@users.noreply.github.com>

--- app/versionsync.js
+++ app/versionsync.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const fs = require('fs');
+import fs from 'fs';
 
-const packageJson = fs.readFileSync('package.json');
+const packageJson = fs.readFileSync('package.json', 'utf8');
 const version = /"version": "(.+)"/.exec(packageJson)[1];
 
-const buildGradle = fs.readFileSync('android/app/build.gradle');
+const buildGradle = fs.readFileSync('android/app/build.gradle', 'utf8');
 const versionCode = /versionCode\s(.+)/.exec(buildGradle)[1];
-const buildGradleWVersionCodeBump = buildGradle.toString().replace(/(\s*versionCode\s)(.*)/, (match, g1, g2) => `${g1}${+g2 + 1}`);
-const buildGradleWVersionSync = buildGradleWVersionCodeBump.replace(/^( +versionName ").*"/, `$1${version}"`);
+const buildGradleWVersionCodeBump = buildGradle.replace(
+  /(\s*versionCode\s)(.*)/,
+  (match, g1, g2) => `${g1}${+g2 + 1}`,
+);
+const buildGradleWVersionSync = buildGradleWVersionCodeBump.replace(
+  /^( +versionName ").*"/,
+  `$1${version}"`,
+);
 fs.writeFileSync('android/app/build.gradle', buildGradleWVersionSync);
 
-console.log(`Version ${version} and versioNCode ${+versionCode + 1} written to build.gradle`)
+console.log(
+  `Version ${version} and versioNCode ${
+    +versionCode + 1
+  } written to build.gradle`,
+);
