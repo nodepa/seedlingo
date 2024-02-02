bbcd3891854e5db59e6e9660382131f6aa91944e
toshify <4579559+toshify@users.noreply.github.com>
2024-02-02 15:40:17 +0100

    feat: Add tracking of app version (#466)
    Because we need to rule out issues that emerge in earlier app versions,
    
    this commit will:
    - add tracking of app version
    - add tracking of whether the app is run in a browser or wrapped as app
    
    **Certification**
    - [X] I certify that <!-- Check the box to certify: [X] -->
    - I have read the [contributing guidelines](
      https://github.com/nodepa/seedlingo/blob/main/.github/CONTRIBUTING.md)
    - I license these contributions to the public under Seedlingo's
      [LICENSE](https://github.com/nodepa/seedlingo/blob/main/LICENSE.md)
      and have the rights to do so.
    
    Signed-off-by: toshify <4579559+toshify@users.noreply.github.com>

--- app/src/common/plugins/PlausibleAnalytics.ts
+++ app/src/common/plugins/PlausibleAnalytics.ts
@@ -1,42 +1,9 @@
 import Plausible from 'plausible-tracker';
-import type { PlausibleOptions } from 'plausible-tracker';
+import type { PlausibleOptions, EventOptions } from 'plausible-tracker';
 import type { App } from 'vue';
 import { inject } from 'vue';
-
-interface ScriptLoaderOption extends Partial<HTMLScriptElement> {
-  'data-domain': string;
-  partytown: boolean;
-}
-
-function loadScript(
-  source: string,
-  options: ScriptLoaderOption = {} as ScriptLoaderOption,
-) {
-  return new Promise((resolve, reject) => {
-    const head = document.head || document.getElementsByTagName('head')[0];
-    const script = document.createElement('script');
-    const {
-      src,
-      type = options.partytown ? 'text/partytown' : 'text/javascript',
-      defer = false,
-      async = false,
-      ...restAttrs
-    } = options;
-    script.type = type;
-    script.defer = defer;
-    script.async = async;
-    script.src = src || source;
-    script.setAttribute('data-domain', options['data-domain']);
-
-    Object.keys(restAttrs).forEach((key) => {
-      (script as any)[key] = (restAttrs as any)[key];
-    });
-
-    head.appendChild(script);
-    script.onload = resolve;
-    script.onerror = reject;
-  });
-}
+import { isPlatform } from '@ionic/vue';
+import { App as CapacitorApp } from '@capacitor/app';
 
 export type IPlausible = typeof Plausible;
 export type ReturnUsePlausible = Omit<
@@ -45,83 +12,100 @@ export type ReturnUsePlausible = Omit<
 >;
 
 export interface OptionPlugin {
-  /**
-   * Plausible options
-   * @type PlausibleOptions
-   */
   init: PlausibleOptions;
-
-  /**
-   * Plausible Settings
-   * @type InstallOptions
-   */
   settings: InstallOptions;
-
-  /**
-   * Partytown support
-   * @default false
-   * @type boolean
-   * @link https://partytown.builder.io/how-does-partytown-work
-   */
   partytown?: boolean;
 }
 
 export interface InstallOptions {
-  /**
-   * Enables automatic page view tracking for SPAs
-   * @default false
-   * @see https://github.com/plausible/plausible-tracker
-   * @type boolean
-   */
   enableAutoPageviews?: boolean;
-  /**
-   * Outbound link click tracking
-   * @default false
-   * @see https://plausible.io/docs/outbound-link-click-tracking
-   * @type boolean
-   *
-   */
   enableAutoOutboundTracking?: boolean;
 }
 
-export function createPlausible(options: OptionPlugin) {
-  const plausible = {
-    install(app: App): void {
-      const data = {
-        ...options.init,
-        apiHost: 'https://plausible.io',
-        enableAutoPageviews: true,
-      } as OptionPlugin['init'];
-      const plausible = Plausible(data);
+type TrackPageview = (
+  eventData?: PlausibleOptions,
+  options?: EventOptions,
+) => void;
 
-      if (options.settings.enableAutoPageviews === true)
-        plausible.enableAutoPageviews();
-
-      if (options.settings.enableAutoOutboundTracking === true)
-        plausible.enableAutoOutboundTracking();
-      let trackLocalhost = '';
-      if (data.trackLocalhost) {
-        trackLocalhost = '.local';
+const enableAutoPageviews = async (
+  trackPageview: TrackPageview,
+  hashMode: boolean,
+) => {
+  const props: { AppVersion?: string; MobileApp?: boolean } = {};
+  props.MobileApp = isPlatform('capacitor');
+  if (props.MobileApp) {
+    const appInfo = await CapacitorApp.getInfo();
+    if (appInfo) {
+      props.AppVersion = `v${appInfo.version}_${appInfo.build}`;
+    } else if (__APP_VERSION__) {
+      props.AppVersion = `v${__APP_VERSION__}`;
+      if (__AWS_JOB_ID__) {
+        props.AppVersion += `_${__AWS_JOB_ID__}`;
       }
-      loadScript(`${data.apiHost}/js/script${trackLocalhost}.js`, {
-        defer: true,
-        'data-domain': data.domain || 'https://plausible.io',
-        partytown: options.partytown || false,
-      });
+      if (__AWS_BRANCH__) {
+        props.AppVersion += ` (${__AWS_BRANCH__})`;
+      }
+    }
+  }
+  const pageview = () =>
+    trackPageview(
+      {},
+      {
+        props,
+      },
+    );
+
+  const originalPushState = history.pushState;
+  if (originalPushState) {
+    history.pushState = function (data, title, url) {
+      originalPushState.apply(this, [data, title, url]);
+      pageview();
+    };
+    addEventListener('popstate', pageview);
+  }
+
+  if (hashMode) {
+    addEventListener('hashchange', pageview);
+  }
+
+  pageview();
+
+  return function cleanup() {
+    if (originalPushState) {
+      history.pushState = originalPushState;
+      removeEventListener('popstate', pageview);
+    }
+    if (hashMode) {
+      removeEventListener('hashchange', pageview);
+    }
+  };
+};
+
+export function createPlausible(options: OptionPlugin) {
+  return {
+    install(app: App): void {
+      const plausible = Plausible(options.init);
+
+      if (options.settings.enableAutoPageviews === true) {
+        // plausible.enableAutoPageviews();
+        enableAutoPageviews(
+          plausible.trackPageview,
+          options.init.hashMode || false,
+        );
+      }
+
+      if (options.settings.enableAutoOutboundTracking === true) {
+        plausible.enableAutoOutboundTracking();
+      }
 
       app.config.globalProperties.$plausible = plausible;
       app.provide('$plausible', plausible);
     },
   };
-  return plausible;
 }
 
 export function usePlausible() {
-  const plausible = inject('$plausible') as ReturnUsePlausible;
-
-  return {
-    ...plausible,
-  };
+  return inject('$plausible') as ReturnUsePlausible;
 }
 
 declare module '@vue/runtime-core' {
