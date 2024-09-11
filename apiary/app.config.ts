d37d6de4f5b032d01e6d6a2055894f309a5c3973
toshify <4579559+toshify@users.noreply.github.com>
2024-09-11 11:29:40 +0200

    feat: Add content editor module
    Because the barrier to creating and editing content should be low
    to enable quick and abundant availability of relevant training material
    to relevant end-users,
    
    this commit will:
    - add a content editor module
    
    **Certification**
    - [X] I certify that <!-- Check the box to certify: [X] -->
    - I have read the [contributing guidelines](
      https://github.com/nodepa/seedlingo/blob/main/.github/CONTRIBUTING.md)
    - I license these contributions to the public under Seedlingo's
      [LICENSE](https://github.com/nodepa/seedlingo/blob/main/LICENSE.md)
      and have the rights to do so.
    
    Signed-off-by: toshify <4579559+toshify@users.noreply.github.com>

---
+++ apiary/app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  title: 'Seedlingo Apiary',
+  ui: {
+    primary: "lochmara",
+    gray: 'cool',
+  }
+})
---
+++ apiary/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig();
+useHeadSafe({
+  title: appConfig.title
+})
+useSeoMeta({
+  title: appConfig.title,
+  ogTitle: appConfig.title,
+  description: 'Content editor for Seedlingo.',
+  ogDescription: 'Content editor for Seedlingo.',
+})
+</script>
